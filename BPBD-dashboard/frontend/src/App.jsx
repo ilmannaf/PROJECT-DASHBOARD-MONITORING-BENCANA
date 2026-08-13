0cbb6931a0b0dba4dad30acb19fb88402c3f3811
ilmannaf <ilmannafia2006@gmail.com>
2026-08-13 14:28:14 +0700

    Perbaiki lokasi instalasi Tailwind, tambah .gitignore root, setup ulang frontend

---
+++ BPBD-dashboard/frontend/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-blue-500">
+      <h1 className="text-white text-3xl font-bold">Tailwind Berhasil!</h1>
+    </div>
+  );
+}
+
+export default App;
